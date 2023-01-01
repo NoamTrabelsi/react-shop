1a0bc14ee4231f7a628642441b9464cc1b85096e
NoamTrabelsi <93206473+NoamTrabelsi@users.noreply.github.com>
2023-01-01 18:34:14 +0200

    add section

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <Nav />
-        <Section
+      <Section
         data={ArrOfProdacts}
-        />
+      />
     </div>
   );
 }
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -2,15 +2,17 @@ import Products from './Products/Products'
 import './Section.css'
 const Section = ({ data }) => {
   return (
-    <section className="products">
-      {data.map((prod) => (
-        <Products
-          price={prod.price}
-          title={prod.title}
-          imageUrl={prod.image}
-        />
-      ))}
-    </section >
+    <div>
+      <section className="products">
+        {data.map((prod) => (
+          <Products
+            price={prod.price}
+            title={prod.title}
+            imageUrl={prod.image}
+          />
+        ))}
+      </section >
+    </div>
   )
 }
 export default Section
